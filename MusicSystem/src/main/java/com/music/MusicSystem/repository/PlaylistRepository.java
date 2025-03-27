09e57255c46a5ffdf36d7b408cd650e9d707f94e
Sreejha Jagadees <sreejhajagadees@gmail.com>
2025-03-27 12:07:43 +0530

    removed wrong files

--- MusicSystem/src/main/java/com/music/MusicSystem/repository/PlaylistRepository.java
+++
@@ -1,9 +0,0 @@
-package com.music.MusicSystem.repository;
-
-import com.music.MusicSystem.model.Playlist;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
-    List<Playlist> findByLanguage(String language);
-}
--- MusicSystem/src/main/java/com/music/MusicSystem/repository/SongRepository.java
+++
@@ -1,9 +0,0 @@
-package com.music.MusicSystem.repository;
-
-import com.music.MusicSystem.model.Song;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface SongRepository extends JpaRepository<Song, Long> {
-    List<Song> findByLanguage(String language);
-}
